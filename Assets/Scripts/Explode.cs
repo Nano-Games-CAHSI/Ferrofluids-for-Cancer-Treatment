58263f4fb921e2fec46ba204db4c9fa41311343d
Nicole Román-Correa <nicole.roman7@upr.edu>
2023-02-10 20:38:51 -0400

    Main setup, UI Mockup, and shooting feature added.

---
+++ Assets/Scripts/Explode.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour
+{
+    public GameObject explosion;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Tumor")
+        {
+            Instantiate(explosion, collision.transform.position, collision.transform.rotation);
+        }
+    }
+}
